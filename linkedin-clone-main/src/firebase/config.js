2e957cbfd291e0cebff403bce1416ec9de49a874
HariniS007 <89060656+HariniS007@users.noreply.github.com>
2023-04-02 10:36:29 +0530

    Add files via upload

---
+++ linkedin-clone-main/src/firebase/config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCESCv06rwheIY0NRMXkSVyquDICcT6wYU",
+  authDomain: "it-park-ce9ef.firebaseapp.com",
+  projectId: "it-park-ce9ef",
+  storageBucket: "it-park-ce9ef.appspot.com",
+  messagingSenderId: "774724910663",
+  appId: "1:774724910663:web:3ef04e0bb5fe7697837b62",
+  measurementId: "G-2BC9Q5YVNP"
+};
